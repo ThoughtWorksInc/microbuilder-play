cebeb4176e458231fe7a27bf57ee99eeef1deec7
Yang Bo <atryyang@thoughtworks.com>
2015-08-05 22:12:32 +0800

    Depends on Haxe files instead of Scala files

--- src/main/scala/RpcPlay.scala
+++ src/main/scala/RpcPlay.scala
@@ -1,3 +1,5 @@
+import com.thoughtworks.restRpc.core.Parser
+
 object RpcPlay {
-  def identity() = s"the rest-rpc-play depends on ${RpcCore.identity}"
+  def identity() = s"the rest-rpc-play depends on ${Parser.identity}"
 }
