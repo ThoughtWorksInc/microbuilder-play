f36f00353428b226489a55308633ba487545dee7
Suo Qin <kpsesuo@gmail.com>
2015-08-21 17:41:29 +0800

    add a fake route in test

--- src/main/scala/com/thoughtworks/restRpc/play/PlayOutgoingJsonService.scala
+++ src/main/scala/com/thoughtworks/restRpc/play/PlayOutgoingJsonService.scala
@@ -3,7 +3,7 @@ package com.thoughtworks.restRpc.play
 import com.qifun.jsonStream.JsonStream
 import com.qifun.jsonStream.io.{PrettyTextPrinter, TextParser}
 import com.qifun.jsonStream.rpc.{ICompleteHandler1, IFuture1, IJsonService}
-import com.thoughtworks.restRpc.core.RouteConfiguration
+import com.thoughtworks.restRpc.core.{UriTemplate, RouteConfiguration}
 import play.api.Logger
 import play.api.libs.json.{JsArray, JsObject, Json}
 
@@ -32,53 +32,21 @@ class PlayOutgoingJsonService(host: String, routes: RouteConfiguration) extends 
   def apply(request: com.qifun.jsonStream.JsonStream, responseHandler: com.qifun.jsonStream.rpc.IJsonResponseHandler): Unit = {
     clientRunning {
       client =>
-        client.url(generateURL(request)).get() map {
+        val template: UriTemplate = routes.nameToUriTemplate("")
+        val uri: String = template.render(request)
+        client.url(s"${host}/${uri}").get() map {
           res => responseHandler.onSuccess(TextParser.parseString(res.json.toString()))
         }
 
     }
   }
-    //By JsonStreamExtractor
-  def generateURL(request: com.qifun.jsonStream.JsonStream): String = {
-    val metaData = Map("myMethod" -> "my-method")
 
-    request match {
+  def extractRouteName(request: JsonStream): String = request match {
       case JsonStreamExtractor.Object(pairs) => {
-        pairs.map {
-          a =>
-            s"${host}/${metaData.getOrElse(a.key, "error")}" + fillInParams(a.value, "/%s/name/%s")
-        }.mkString
+        pairs.map(_.key).mkString
       }
       case _ => ""
     }
-  }
-
-  //By Json.parse
-  def generateURL2(request: com.qifun.jsonStream.JsonStream): String = {
-    val metaData = Map("myMethod" -> "my-method")
-
-    val requestJsonData: JsObject = Json.parse(PrettyTextPrinter.toString(request)).as[JsObject]
-    val methodName: Set[String] = requestJsonData.keys
-    val params: JsArray = requestJsonData.value.get(methodName.head).get.as[JsArray]
-    s"${host}/${metaData.getOrElse(methodName.head, "error")}" + "/%s/name/%s".format(params.value : _*).replaceAll("\"", "")
-  }
-
-  def fillInParams(params: com.qifun.jsonStream.JsonStream, urlWithPlaceHolder: String): String = {
-    params match {
-      case JsonStreamExtractor.Array(elements) => {
-        val params = elements.toStream.map {
-          case JsonStreamExtractor.Integer(number) =>
-            number.toString
-          case JsonStreamExtractor.String(json) =>
-            json
-          case _ => ""
-        }
-        urlWithPlaceHolder.format(params: _*)
-      }
-      case _ => ""
-    }
-
-  }
 
   def clientRunning(callback: ((play.api.libs.ws.ning.NingWSClient) => Future[Unit])) = {
     val builder = new com.ning.http.client.AsyncHttpClientConfig.Builder()
--- src/test/scala/com/thoughtworks/restRpc/play/RpcOutgoingTest.scala
+++ src/test/scala/com/thoughtworks/restRpc/play/RpcOutgoingTest.scala
@@ -3,7 +3,7 @@ package com.thoughtworks.restRpc.play
 import java.util.concurrent.TimeUnit.SECONDS
 
 import com.github.dreamhead.moco._
-import com.thoughtworks.restRpc.play.Implicits._
+import com.thoughtworks.restRpc.core.{RouteConfiguration, UriTemplate}
 import org.mockito.Mockito
 import org.specs2.Specification
 import org.specs2.mock.{Mockito => SpecMockito}
@@ -11,8 +11,10 @@ import org.specs2.mock.{Mockito => SpecMockito}
 import scala.concurrent._
 import scala.concurrent.duration._
 import scala.language.implicitConversions
+import com.thoughtworks.restRpc.play.Implicits.jsonStreamFutureToScalaFuture
 
-class RpcOutgoingTest extends Specification with SpecMockito {def is = s2"""
+class RpcOutgoingTest extends Specification with SpecMockito {
+  def is = s2"""
 
       This is a specification of using rest-rpc-play tools to make http requests
 
@@ -32,11 +34,11 @@ class RpcOutgoingTest extends Specification with SpecMockito {def is = s2"""
 
 
 
-    val myRpc:MyRpc = MyOutgoingProxyFactory.outgoingProxy_com_thoughtworks_restRpc_play_MyRpc(
-      new PlayOutgoingJsonService("http://localhost:8090", MyUriTemplateProcessor.processor_com_thoughtworks_restRpc_play_MyRpc)
+    val myRpc: MyRpc = MyOutgoingProxyFactory.outgoingProxy_com_thoughtworks_restRpc_play_MyRpc(
+      new PlayOutgoingJsonService("http://localhost:8090", configuration)
     )
 
-    val scalaResponseFuture:Future[MyResponse] = myRpc.myMethod(1, "abc")
+    val scalaResponseFuture: Future[MyResponse] = myRpc.myMethod(1, "abc")
 
 
     val response = Await.result(scalaResponseFuture, Duration(5, SECONDS))
@@ -54,11 +56,11 @@ class RpcOutgoingTest extends Specification with SpecMockito {def is = s2"""
             }
           }""")
 
-    val myRpc:MyRpc = MyOutgoingProxyFactory.outgoingProxy_com_thoughtworks_restRpc_play_MyRpc(
-      new PlayOutgoingJsonService("http://localhost:8090", MyUriTemplateProcessor.processor_com_thoughtworks_restRpc_play_MyRpc)
+    val myRpc: MyRpc = MyOutgoingProxyFactory.outgoingProxy_com_thoughtworks_restRpc_play_MyRpc(
+      new PlayOutgoingJsonService("http://localhost:8090", configuration)
     )
 
-    val scalaResponseFuture:Future[MyResponse] = myRpc.myMethod(2, "def")
+    val scalaResponseFuture: Future[MyResponse] = myRpc.myMethod(2, "def")
 
 
     val response = Await.result(scalaResponseFuture, Duration(5, SECONDS))
@@ -78,4 +80,49 @@ class RpcOutgoingTest extends Specification with SpecMockito {def is = s2"""
     theServer.start()
     theServer
   }
+
+  def configuration: RouteConfiguration = {
+    new RouteConfiguration() {
+      override def nameToUriTemplate(name: String): UriTemplate = {
+        new UriTemplate {
+          val metaData = Map("myMethod" -> "my-method")
+
+          override def render(context: scala.Any): String = generateURL(context)
+
+          //By JsonStreamExtractor
+          def generateURL(request: scala.Any): String = {
+            request match {
+              case JsonStreamExtractor.Object(pairs) => {
+                pairs.map {
+                  a =>
+                    s"${metaData.getOrElse(a.key, "error")}" + fillInParams(a.value, "/%s/name/%s")
+                }.mkString
+              }
+              case _ => ""
+            }
+          }
+
+          def fillInParams(params: com.qifun.jsonStream.JsonStream, urlWithPlaceHolder: String): String = {
+            params match {
+              case JsonStreamExtractor.Array(elements) => {
+                val params = elements.toStream.map {
+                  case JsonStreamExtractor.Integer(number) =>
+                    number.toString
+                  case JsonStreamExtractor.String(json) =>
+                    json
+                  case _ => ""
+                }
+                urlWithPlaceHolder.format(params: _*)
+              }
+              case _ => ""
+            }
+
+          }
+
+          override def method(): String = "GET"
+        }
+      }
+    }
+  }
+
 }
