fa0f2f130e15a2554b9e4c017b71d35e9b6db2dc
Suo Qin <kpsesuo@gmail.com>
2015-08-21 11:26:31 +0800

    refactor, create clientRunning helper

--- src/main/scala/com/thoughtworks/restRpc/play/PlayOutgoingJsonService.scala
+++ src/main/scala/com/thoughtworks/restRpc/play/PlayOutgoingJsonService.scala
@@ -13,10 +13,10 @@ import scala.concurrent._
 
 
 object Implicits {
-  implicit def jsonStreamFutureToScalaFuture[Value](jsonStreamFuture: IFuture1[Value]):Future[Value] = {
+  implicit def jsonStreamFutureToScalaFuture[Value](jsonStreamFuture: IFuture1[Value]): Future[Value] = {
     val p = promise[Value]
 
-    jsonStreamFuture.start(new ICompleteHandler1[Value]{
+    jsonStreamFuture.start(new ICompleteHandler1[Value] {
       override def onFailure(o: scala.Any): Unit =
         p failure new Exception
 
@@ -31,24 +31,30 @@ class PlayOutgoingJsonService(host: String, routes: RouteConfiguration) extends 
 
   def apply(request: com.qifun.jsonStream.JsonStream, responseHandler: com.qifun.jsonStream.rpc.IJsonResponseHandler): Unit = {
 
-    val client = {
-      val builder = new com.ning.http.client.AsyncHttpClientConfig.Builder()
-      new play.api.libs.ws.ning.NingWSClient(builder.build())
-    }
+    val requestJsonData: JsObject = Json.parse(PrettyTextPrinter.toString(request)).as[JsObject]
 
-    val string = PrettyTextPrinter.toString(request)
-    val json: JsObject = Json.parse(string).as[JsObject]
-
-    val methodName : Set[String] = json.keys
-    val resourceName = Map("myMethod" -> "my-method")
-    val params : JsArray = json.value.get(methodName.head).get.as[JsArray]
-    val url: String = s"${host}/${resourceName.getOrElse(methodName.head, "error")}/${params(0)}/name/${params(1).as[String]}"
-    client.url(url).get() map {
-      res =>
-        val raw: JsonStream = TextParser.parseString(res.json.toString())
-        responseHandler.onSuccess(raw)
-        client.close()
+    val methodName: Set[String] = requestJsonData.keys
+    val metaData = Map("myMethod" -> "my-method")
+    clientRunning {
+      client =>
+        client.url(generateURL(requestJsonData, methodName, metaData)).get() map {
+          res =>
+            val raw: JsonStream = TextParser.parseString(res.json.toString())
+            responseHandler.onSuccess(raw)
+        }
     }
+  }
+
+  def generateURL(requestJsonData: JsObject, methodName: Set[String], metaData: Map[String, String]): String = {
+    val params: JsArray = requestJsonData.value.get(methodName.head).get.as[JsArray]
+    s"${host}/${metaData.getOrElse(methodName.head, "error")}/${params(0)}/name/${params(1).as[String]}"
+  }
+
+  def clientRunning(callback: ((play.api.libs.ws.ning.NingWSClient) => Future[Unit])) = {
+    val builder = new com.ning.http.client.AsyncHttpClientConfig.Builder()
+    val client = new play.api.libs.ws.ning.NingWSClient(builder.build())
+    callback(client).onComplete(_ => client.close())
+
   }
 
   def push(x$1: com.qifun.jsonStream.JsonStream): Unit = {
