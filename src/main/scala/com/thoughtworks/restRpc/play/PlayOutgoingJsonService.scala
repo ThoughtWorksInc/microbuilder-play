1898a4f4ebbdb0982e610e770b95f85cf8da13b8
Suo Qin <kpsesuo@gmail.com>
2015-08-21 11:07:38 +0800

    pass the resource parameters test

--- src/main/scala/com/thoughtworks/restRpc/play/PlayOutgoingJsonService.scala
+++ src/main/scala/com/thoughtworks/restRpc/play/PlayOutgoingJsonService.scala
@@ -1,11 +1,13 @@
 package com.thoughtworks.restRpc.play
 
 import com.qifun.jsonStream.JsonStream
-import com.qifun.jsonStream.io.TextParser
+import com.qifun.jsonStream.io.{PrettyTextPrinter, TextParser}
 import com.qifun.jsonStream.rpc.{ICompleteHandler1, IFuture1, IJsonService}
 import com.thoughtworks.restRpc.core.RouteConfiguration
 import play.api.Logger
+import play.api.libs.json.{JsArray, JsObject, Json}
 
+import scala.collection.Set
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 
@@ -28,13 +30,19 @@ object Implicits {
 class PlayOutgoingJsonService(host: String, routes: RouteConfiguration) extends IJsonService {
 
   def apply(request: com.qifun.jsonStream.JsonStream, responseHandler: com.qifun.jsonStream.rpc.IJsonResponseHandler): Unit = {
-    // Standard Play-style WSAPI
+
     val client = {
       val builder = new com.ning.http.client.AsyncHttpClientConfig.Builder()
       new play.api.libs.ws.ning.NingWSClient(builder.build())
     }
-//    val wsAPI: WSAPI = new StandaloneWSAPI()
-    val url: String = s"${host}/my-method/1/name/abc"
+
+    val string = PrettyTextPrinter.toString(request)
+    val json: JsObject = Json.parse(string).as[JsObject]
+
+    val methodName : Set[String] = json.keys
+    val resourceName = Map("myMethod" -> "my-method")
+    val params : JsArray = json.value.get(methodName.head).get.as[JsArray]
+    val url: String = s"${host}/${resourceName.getOrElse(methodName.head, "error")}/${params(0)}/name/${params(1).as[String]}"
     client.url(url).get() map {
       res =>
         val raw: JsonStream = TextParser.parseString(res.json.toString())
